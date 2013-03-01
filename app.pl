f0c8834d491b5592aba16586261e7b7362b45482
James Dennes <jdennes@gmail.com>
2013-03-01 12:01:04 +0100

    Go.

---
+++ app.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/env perl
+use Mojolicious::Lite;
+use lib 'lib';
+use Net::CampaignMonitor;
+
+plugin 'PODRenderer';
+
+get '/' => sub {
+  my $self = shift;
+  my $url = Net::CampaignMonito->authorize_url(
+    client_id => $ENV{CREATESEND_CLIENT_ID},
+    redirect_uri => $ENV{CREATESEND_REDIRECT_URI},
+    scope => 'ViewReports,CreateCampaigns,SendCampaigns'
+  );
+  return $self->redirect_to();
+};
+
+get '/exchange_token' => sub {
+  my $self = shift;
+  my $response = 'TODO: Exchange token.';
+  $self->render(text => $response);
+};
+
+app->start;
