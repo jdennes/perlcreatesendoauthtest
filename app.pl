daae9bbd503746a3a4b674dbe231139e6cf662e7
James Dennes <jdennes@gmail.com>
2013-03-01 12:44:15 +0100

    Implemented /exchange_token.

--- app.pl
+++ app.pl
@@ -2,6 +2,7 @@
 use Mojolicious::Lite;
 use lib 'lib';
 use Net::CampaignMonitor;
+use Data::Dumper;
 
 plugin 'PODRenderer';
 
@@ -17,7 +18,29 @@ get '/' => sub {
 
 get '/exchange_token' => sub {
   my $self = shift;
-  my $response = 'TODO: Exchange token.';
+  my $code = $self->param('code');
+  
+  my $token_details = Net::CampaignMonitor->exchange_token(
+    client_id => $ENV{CREATESEND_CLIENT_ID},
+    client_secret => $ENV{CREATESEND_CLIENT_SECRET},
+    redirect_uri => $ENV{CREATESEND_REDIRECT_URI},
+    code => $code
+  );
+
+  my $response = "Your user is successfully authenticated. Here are the details you need:<br/><br/>";
+  $response .= "access token: ".$token_details->{access_token}."<br/>";
+  $response .= "refresh token: ".$token_details->{refresh_token}."<br/>";
+  $response .= "expires in: ".$token_details->{expires_in}."<br/>";
+  $response .= "<br/><br/>";
+
+  my $cm = Net::CampaignMonitor->new(
+    access_token => $token_details->{access_token},
+    refresh_token => $token_details->{refresh_token}
+  );
+  my $clients = $cm->account_clients();
+
+  $response .= "We've made an API call too. Here are your clients:<br/><br/>";
+  $response .= Dumper($clients);
   $self->render(text => $response);
 };
 
